c982ea2bc8b5e735d26f9b55697fdf900d25daa6
Tojosoa Nantenaina Raveloarijaona <ravelnant@gmail.com>
2021-01-31 03:50:57 -0700

    Update Calcul_basique_de_pi.py

--- Monte Carlo/Calcul_basique_de_pi.py
+++ Monte Carlo/Calcul_basique_de_pi.py
@@ -1,9 +1,12 @@
 import random
-
-essaie = 4000
-tirs = 0
-for iter in range(essaie):
-    x, y = random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0)
-    if x**2 + y**2 < 1.0: 
-        tirs += 1
-print (4.0 * tirs / float(essaie))
+ 
+def direct_pi(N):
+    n_hits = 0
+    for i in range(N):
+        x, y = random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0)
+        if x ** 2 + y ** 2 < 1.0:
+            n_hits += 1
+    return n_hits
+ 
+n_trials = 10000000
+print (4.0 * direct_pi(n_trials) / float(n_trials))
